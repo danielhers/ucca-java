c707714f820c8897a4d9640fea33c911fec1d3f8
Daniel Hershcovich <danielh@cs.huji.ac.il>
2014-08-27 15:01:27 +0300

    Use actual UCCA passages as corpus and predictions

--- src/il/ac/huji/cs/nlp/ucca/UccaRntn.java
+++ src/il/ac/huji/cs/nlp/ucca/UccaRntn.java
@@ -1,9 +1,14 @@
 package il.ac.huji.cs.nlp.ucca;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
+import javax.xml.bind.JAXBException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.math3.random.MersenneTwister;
 import org.deeplearning4j.nn.activation.Activations;
 import org.deeplearning4j.rntn.RNTN;
@@ -20,45 +25,80 @@ import org.slf4j.LoggerFactory;
 public class UccaRntn {
 
     private static Logger log = LoggerFactory.getLogger(UccaRntn.class);
-	private static String sentence = "<LABEL> This is one sentence. </LABEL>";
 
 	private TreeVectorizer vectorizer;
 	private Word2Vec vec;
 	private RNTN rntn;
 	private SentenceIterator sentenceIter;
 
-	public UccaRntn() throws Exception {
+	public UccaRntn(List<Passage> passages) throws Exception {
 		vectorizer = new TreeVectorizer();
-		sentenceIter = new CollectionSentenceIterator(Arrays.asList(sentence));
-		File vecModel = new File("wordvectors.ser");
-		if (vecModel.exists()) {
-			vec = (Word2Vec) SerializationUtils.readObject(vecModel);
-		} else {
-			vec = new Word2Vec(sentenceIter);
-			vec.train();
-			log.info("Saving word2vec model...");
-			SerializationUtils.saveObject(vec, vecModel);
-		}
+		sentenceIter = new CollectionSentenceIterator(getPassageTexts(passages));
+		vec = getWord2VecModel(sentenceIter);
 		rntn = new RNTN.Builder().setActivationFunction(Activations.tanh())
 				.setAdagradResetFrequency(1).setCombineClassification(true).setFeatureVectors(vec)
 				.setRandomFeatureVectors(false).setRng(new MersenneTwister(123))
 				.setUseTensors(true).setNumHidden(25).build();
 	}
 
-	private void train() throws Exception {
-        List<Tree> trees = vectorizer.getTreesWithLabels(sentence,Arrays.asList("LABEL","NONE"));
+	private static Word2Vec getWord2VecModel(SentenceIterator sentenceIter) {
+		File modelDir = new File("models");
+		modelDir.mkdir();
+		File vecModel = new File(modelDir, "wordvectors.ser");
+		if (vecModel.exists()) {
+			return (Word2Vec) SerializationUtils.readObject(vecModel);
+		}
+		Word2Vec vec = new Word2Vec(sentenceIter);
+		vec.train();
+		log.info("Saving word2vec model...");
+		SerializationUtils.saveObject(vec, vecModel);
+		return vec;
+	}
+
+	private static List<String> getPassageTexts(List<Passage> passages) {
+		List<String> passageTexts = new ArrayList<String>();
+		for (Passage passage : passages) {
+			passageTexts.add(passage.getText());
+		}
+		return passageTexts;
+	}
+
+	private void train(List<Passage> passages) throws Exception {
+        List<Tree> trees = vectorizer.getTreesWithLabels(
+        		StringUtils.join(passages, "\\n"), getAllLabels(passages));
         rntn.train(trees);
 	}
 
-	private List<Integer> predict() throws Exception {
-        List<Tree> trees = vectorizer.getTreesWithLabels(sentence,Arrays.asList("LABEL","NONE"));
-		return rntn.predict(trees);
+	private ArrayList<String> getAllLabels(List<Passage> passages) {
+		Set<String> labels = new TreeSet<String>();
+		for (Passage passage : passages) {
+			labels.addAll(passage.getAllEdgeTypes());
+		}
+		return new ArrayList<String>(labels);
+	}
+
+	private List<Tree> predict(List<Passage> passages) throws Exception {
+        List<Tree> trees = vectorizer.getTrees(StringUtils.join(passages, "\\n"));
+        for (Tree tree : trees) {
+    		rntn.forwardPropagateTree(tree);
+		}
+		return trees;
 	}
 
 	public static void main(String[] args) throws Exception {
-		UccaRntn rntn = new UccaRntn();
-		rntn.train();
-		System.out.println(rntn.predict());
+		List<Passage> passages = readPassages("../ucca/corpus");
+		UccaRntn rntn = new UccaRntn(passages);
+		rntn.train(passages);
+		System.out.println(rntn.predict(passages));
+	}
+
+	private static List<Passage> readPassages(String path) throws JAXBException {
+		List<Passage> passages = new ArrayList<Passage>();
+		File corpusDir = new File(path);
+		for (File file : corpusDir.listFiles()) {
+			passages.add(Passage.read(file));
+		}
+		return passages;
 	}
 
 }
